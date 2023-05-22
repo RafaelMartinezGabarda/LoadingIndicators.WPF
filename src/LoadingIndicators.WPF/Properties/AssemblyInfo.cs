f7c997cfdc8ce0af41f091b95b0cd274c13f6fa1
Rafael Martinez Gabarda <ramartinezga1@acciona.com>
2023-05-22 14:54:38 +0200

    Rename and stuff

--- src/LoadingIndicators.WPF/Properties/AssemblyInfo.cs
+++ src/LoadingIndicators.WPF/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly:XmlnsPrefix("http://github.com/zeluisping/loadingIndicators/xaml/controls", "li")]
-[assembly:XmlnsDefinition("http://github.com/zeluisping/loadingIndicators/xaml/controls", "LoadingIndicators.WPF")]
+[assembly: XmlnsPrefix("http://github.com/zeluisping/loadingIndicators/xaml/controls", "li")]
+[assembly: XmlnsDefinition("http://github.com/zeluisping/loadingIndicators/xaml/controls", "LoadingIndicators.WPF")]
